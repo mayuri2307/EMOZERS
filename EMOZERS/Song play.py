28443e5d05867e141d545875382f703ef46c4e81
Mayuri Gupta <mg153@snu.edu.in>
2019-03-24 17:35:44 +0530

    HackData 2.0 Project-- Music player which analysis emotions and plays the song

--- EMOZERS/Song play.py
+++
@@ -1,2 +0,0 @@
-import playsound
-playsound.playsound('C:\\Users\\Siddharth\\Music\\The Beatles - 1 (Greatest Hits) [2000] [320 kbps]\\Love me do.mp3', True)
--- EMOZERS/VLCpauseSong.py
+++
@@ -1,3 +0,0 @@
-import vlc
-p = vlc.MediaPlayer("C:\\Users\\Siddharth\\Music\\The Beatles - 1 (Greatest Hits) [2000] [320 kbps]\\Love me do.mp3")
-p.stop()
